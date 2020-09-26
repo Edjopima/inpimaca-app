0596140ca3ff480616caaa50630538ec28b3ec14
Eduardo Piña <mrejpm@gmail.com>
2020-09-26 08:20:39 -0400

    ajuste de detalles en menu lateral"

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,17 +1,20 @@
 import React from 'react';
 import './Header.css'
+import logo from './LOGO.JPG'
 
 const Header = () =>{
     return(
         <nav className = 'header'>
+                <img src={logo} className='logo'/>
                 <p className = 'item' >Inicio</p>
                 <p className = 'item' >Tareas</p>
+                <p className = 'item' >Inventario</p>
                 <p className = 'item' >Deudores</p>
                 <p className = 'item' >Lotes</p>
                 <p className = 'item' >Proveedores</p>
                 <p className = 'item' >Facturas</p>
                 <p className = 'item' >Perfil</p>
-                <p className = 'item signout' >Cerrar sesion</p>
+                <p className = 'item ' >Cerrar sesion</p>
         </nav>
     )
 }
