7dab34d29b4cf7592a49eb77903bdf5f3e6118f6
Eduardo Piña <mrejpm@gmail.com>
2020-11-04 10:09:34 -0400

    Updates

---
+++ precache-manifest.b02ab619e3c06725adff80de9464c91c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e4b32f4afde39b8dac70b5cd6da01c1f",
+    "url": "/inpimaca-app/index.html"
+  },
+  {
+    "revision": "680c45d6887ca77c5ea4",
+    "url": "/inpimaca-app/static/css/main.6580bd00.chunk.css"
+  },
+  {
+    "revision": "f5fd703452648eb2f768",
+    "url": "/inpimaca-app/static/js/2.710abb33.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/inpimaca-app/static/js/2.710abb33.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "680c45d6887ca77c5ea4",
+    "url": "/inpimaca-app/static/js/main.8a0ec268.chunk.js"
+  },
+  {
+    "revision": "d0660fb0f25209cf9e54",
+    "url": "/inpimaca-app/static/js/runtime-main.aad0e998.js"
+  },
+  {
+    "revision": "63121e3f203254b74783d2d95882941f",
+    "url": "/inpimaca-app/static/media/LOGO.63121e3f.JPG"
+  }
+]);
