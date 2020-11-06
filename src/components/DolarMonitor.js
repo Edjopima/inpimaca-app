0293120d4f59fdc649cb9f2c7202f2e7a67b4785
Eduardo Piña <mrejpm@gmail.com>
2020-11-06 08:24:08 -0400

    [Add] dolar today monitor to monitor dolar component

--- src/components/DolarMonitor.js
+++ src/components/DolarMonitor.js
@@ -1,29 +1,32 @@
-import React, {useEffect} from 'react';
+import React, {useEffect,useState} from 'react';
 import './DolarMonitor.css';
 import {useDispatch,useSelector} from 'react-redux';
 
 const MonitorDolar = ()=> {
     const dolar = useSelector((state)=>state.dolar);
     const dispatch = useDispatch();
-
+    const [dolarToday, setDolarToday]=useState(0);
     useEffect(()=>{
         fetch('https://s3.amazonaws.com/dolartoday/data.json')
         .then(response=> response.json())
-        .then(data=> dispatch(
-            (data>dolar)?
-            {
-                type:'SET_DOLAR',
-                payload: data.USD.dolartoday
-            }:{
-                type:'SET_DOLAR',
-                payload:dolar
-            }
-        ))
+        .then(data=> {
+            setDolarToday(data.USD.dolartoday);
+            dispatch(
+                (data.USD.dolartoday>dolar)?
+                {
+                    type:'SET_DOLAR',
+                    payload: data.USD.dolartoday
+                }:{
+                    type:'SET_DOLAR',
+                    payload:dolar
+                }
+        )})
         .catch(err=> console.error(err));
     },[])
     return(
         <div className='MonitorDolar'>
-            <h1>Dolar: {dolar}</h1>
+            <h1>Dolar Compra: {dolar}</h1>
+            <h1>Dolar Today: {dolarToday} </h1>
         </div>
     );
 }
